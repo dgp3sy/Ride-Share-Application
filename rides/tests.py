e87fe68533cec38da2be8a57a7d5eeba6dfc2525
Danny Perkins <dgp3sy>
2019-10-03 16:42:58 -0400

    dummy test case made to test travis CI

--- rides/tests.py
+++ rides/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class DummyTestCase(TestCase):
+    def setUp(self):
+        x = 1
+
+    def test_dummy_test_case(self):
+        self.assertEqual(1, 1)
