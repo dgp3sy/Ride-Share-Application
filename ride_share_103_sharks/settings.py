49ef156cce0579b3cb2d4176b95e5d8e30919cbf
Dan Perkins <48068870+dgp3sy@users.noreply.github.com>
2019-10-14 11:00:03 -0400

    Update settings.py

--- ride_share_103_sharks/settings.py
+++ ride_share_103_sharks/settings.py
@@ -85,10 +85,14 @@ WSGI_APPLICATION = 'ride_share_103_sharks.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+'default': {
+'ENGINE': 'django.db.backends.postgresql',
+'NAME': 'SHARKS',
+'USER': 'ADMIN',
+'PASSWORD': 'PASSWORD',
+'HOST': 'localhost',
+'PORT': '5432',
+}
 }
 
 
