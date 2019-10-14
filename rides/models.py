387b67aec7d95a7bab74d265f2aabb483748a87e
Will Lampert <wjl6ev@virginia.edu>
2019-10-14 18:07:23 -0400

    Started to update the date field for rides

--- rides/models.py
+++ rides/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Ride(models.Model):
     origin = models.CharField(max_length=50)
     destination = models.CharField(max_length=50)
-    departure_date = models.CharField(max_length=50)
+    departure_date = models.DateField()
     seats_available = models.IntegerField()
     def __str__(self):
         return '%s %s %s' % (self.origin, self.destination, self.departure_date)
