66abbc7a2e839b9e4396d0c28eb3154efa41c02c
Danny Perkins <dgp3sy>
2019-10-14 11:20:52 -0400

    travis postgreSQL

--- ride_share_103_sharks/settings.py
+++ ride_share_103_sharks/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'ride_share_103_sharks.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'SHARKS',
+        'USER': 'admin',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
