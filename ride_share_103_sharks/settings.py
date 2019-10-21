2185d3d8534345b77da9e2cb0a4025ad6a8ec660
dl8ve <Dong-L@users.noreply.github.com>
2019-10-21 17:34:18 -0400

    initial commit

--- ride_share_103_sharks/settings.py
+++ ride_share_103_sharks/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'ENGINE' : 'django.db.backends.postgresql',
         'NAME' : 'postgres',
         'USER' : 'postgres',
-        'PASSWORD' : 'password',
+        'PASSWORD' : 'wordpass',
         'HOST' : 'localhost',
         'PORT' : '5432'
     }
