9d338ca45c07911349a772df70906022236c5333
Danny Perkins <dgp3sy>
2019-10-27 16:45:06 -0400

    fixed model

--- rides/models.py
+++ rides/models.py
@@ -7,7 +7,6 @@ class Ride(models.Model):
     destination_state = models.CharField(max_length=50, default="N/A")
     departure_date = models.DateField()
     seats_available = models.IntegerField(choices = [(i,i) for i in range(1,6)])
-    estimated_time = models.CharField(max_length=50, default="Not Available")
     def __str__(self):
         return '%s %s %s' % (self.origin, self.destination, self.departure_date)
 
